bfe55a6504081aafaa40aeab4829f3e3385dc7fb
Abhishek Marathe <abhishek.marathe@ril.com>
2017-09-14 16:20:48 +0530

    Added command line options and added options to file rewriting

--- index.js
+++ index.js
@@ -1,84 +1,109 @@
-var fs=require('fs')
-var _=require('lodash')
-var tabLength=0
-var output=[]
-var finalString=''
-var stack=[]
-fs.readFile(process.argv[2], 'utf8',(err, data) => {
-  if (err) throw err;
-  	var res = data.split("\n");
-  	tabLength=0
-  	for(var i in res){
-  		handleSentence(res[i],i)
-  	}
-  	fs.writeFile(process.argv[2]+"-beutify.js", finalString, 'utf8',(err) => {
-	  if (err) throw err;
-	  console.log('It\'s saved!');
-	});
-  	// console.log('<<<<<<<--output String-->>>>>>>>')
-});
+const fs = require('fs')
+const _ = require('lodash')
+const startIndex = exports;
+const commandLineArgs = require('command-line-args')
 
-function handleSentence(string,i){
+let tabLength = 0
+let output = []
+let finalString = ''
+let stack = []
 
-		var concatenated=false
+const optionDefinitions = [{
+    name: 'rewrite',
+    alias: 'r',
+    type: Boolean
+}, {
+    name: 'filename',
+    alias: 'f',
+    type: String
+}]
 
-		var isHtml=validHTML(string)
-		//console.log(isHtml,string)
-		//console.log(string,)
+const options = commandLineArgs(optionDefinitions, {
+    partial: true
+})
 
-		if(isHtml!=0)
-		{
-			if(isHtml==-1)
-			{
-				tabLength=tabLength-4	
-			}
-			output[i]=_.padStart(string,string.length+tabLength,' ')
-			finalString+=output[i]+'\n';
-			if(isHtml==1)
-			{
-				tabLength=tabLength+4
-			}
-		}
-		else{
-			if(string.indexOf('{')!=-1&&string.indexOf('}')!=-1)
-			{
-				output[i]=_.padStart(string,string.length+tabLength,' ')
-				finalString+=output[i]+'\n';
-			}
-			else{
-				if(string.indexOf('}')!=-1)
-				{
-					tabLength=tabLength-4
-				}
-				output[i]=_.padStart(string,string.length+tabLength,' ')
-				finalString+=output[i]+'\n';
-				if(string.indexOf('{')!=-1)
-				{
-					tabLength=tabLength+4
-				}
+if (!options.filename) {
+    console.log('Pass filename as -f <filename>')
+    console.log('Use -r if you wnat to rewrite file')
+} else {
+    start();
+}
 
-			}
-			
-		}
+function start() {
+
+    fs.readFile(options.filename, 'utf8', (err, data) => {
+        if (err) throw err;
+        var res = data.split("\n");
+        tabLength = 0
+        for (var i in res) {
+            handleSentence(res[i], i)
+        }
+	
+	let path = '';
+	
+	if(options.rewrite){
+		path = options.filename
+	}else{
+		path = options.filename + "-beutify"
+	}    
+	
+	fs.writeFile(path, finalString, 'utf8', (err) => {
+            if (err) throw err;
+            console.log('It\'s saved!');
+        });
+        // console.log('<<<<<<<--output String-->>>>>>>>')
+    });
+}
+
+function handleSentence(string, i) {
+
+    var concatenated = false
+
+    var isHtml = validHTML(string)
+        //console.log(isHtml,string)
+        //console.log(string,)
+
+    if (isHtml != 0) {
+        if (isHtml == -1) {
+            tabLength = tabLength - 4
+        }
+        output[i] = _.padStart(string, string.length + tabLength, ' ')
+        finalString += output[i] + '\n';
+        if (isHtml == 1) {
+            tabLength = tabLength + 4
+        }
+    } else {
+        if (string.indexOf('{') != -1 && string.indexOf('}') != -1) {
+            output[i] = _.padStart(string, string.length + tabLength, ' ')
+            finalString += output[i] + '\n';
+        } else {
+            if (string.indexOf('}') != -1) {
+                tabLength = tabLength - 4
+            }
+            output[i] = _.padStart(string, string.length + tabLength, ' ')
+            finalString += output[i] + '\n';
+            if (string.indexOf('{') != -1) {
+                tabLength = tabLength + 4
+            }
+
+        }
+
+    }
 }
 
 function validHTML(html) {
-  var openingTags, closingTags;
+    var openingTags, closingTags;
 
-  html        = html.replace(/<[^>]*\/\s?>/g, '');      // Remove all self closing tags
-  html        = html.replace(/<(br|hr|img).*?>/g, '');  // Remove all <br>, <hr>, and <img> tags
-  openingTags = html.match(/<[^\/].*?>/g) || [];        // Get remaining opening tags
-  closingTags = html.match(/<\/.+?>/g) || [];           // Get remaining closing tags
-  if(openingTags.length === closingTags.length)
-  {
-  	return 0
-  }
-  else if(openingTags.length > closingTags.length)
-  {
-  	return 1
-  }
-  else{
-  	return -1
-  }
-  //return openingTags.length === closingTags.length ? true : false;
+    html = html.replace(/<[^>]*\/\s?>/g, ''); // Remove all self closing tags
+    html = html.replace(/<(br|hr|img).*?>/g, ''); // Remove all <br>, <hr>, and <img> tags
+    openingTags = html.match(/<[^\/].*?>/g) || []; // Get remaining opening tags
+    closingTags = html.match(/<\/.+?>/g) || []; // Get remaining closing tags
+    if (openingTags.length === closingTags.length) {
+        return 0
+    } else if (openingTags.length > closingTags.length) {
+        return 1
+    } else {
+        return -1
+    }
+    //return openingTags.length === closingTags.length ? true : false;
 }
